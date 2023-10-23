d0345f554294479e29cbe669177787e0061643ca
quangchu <quangchu@amazon.com.vn>
2023-10-23 16:59:39 +0700

    add platform and dev teams

---
+++ EKS_team.md
@@ -0,0 +1,214 @@
+#Add platform tem
+```bash
+cat <<'EOF' >> ./environment/eks-blueprint/modules/eks_cluster/main.tf
+
+data "aws_iam_role" "eks_admin_role_name" {
+  count     = local.eks_admin_role_name != "" ? 1 : 0
+  name = local.eks_admin_role_name
+}
+
+module "eks_blueprints_platform_teams" {
+  source  = "aws-ia/eks-blueprints-teams/aws"
+  version = "~> 0.2"
+
+  name = "team-platform"
+
+  # Enables elevated, admin privileges for this team
+  enable_admin = true
+ 
+  # Define who can impersonate the team-platform Role
+  users             = [
+    data.aws_caller_identity.current.arn,
+    try(data.aws_iam_role.eks_admin_role_name[0].arn, data.aws_caller_identity.current.arn),
+  ]
+  cluster_arn       = module.eks.cluster_arn
+  oidc_provider_arn = module.eks.oidc_provider_arn
+
+  labels = {
+    "elbv2.k8s.aws/pod-readiness-gate-inject" = "enabled",
+    "appName"                                 = "platform-team-app",
+    "projectName"                             = "project-platform",
+  }
+
+  annotations = {
+    team = "platform"
+  }
+
+  namespaces = {
+    "team-platform" = {
+
+      resource_quota = {
+        hard = {
+          "requests.cpu"    = "10000m",
+          "requests.memory" = "20Gi",
+          "limits.cpu"      = "20000m",
+          "limits.memory"   = "50Gi",
+          "pods"            = "20",
+          "secrets"         = "20",
+          "services"        = "20"
+        }
+      }
+
+      limit_range = {
+        limit = [
+          {
+            type = "Pod"
+            max = {
+              cpu    = "1000m"
+              memory = "1Gi"
+            },
+            min = {
+              cpu    = "10m"
+              memory = "4Mi"
+            }
+          },
+          {
+            type = "PersistentVolumeClaim"
+            min = {
+              storage = "24M"
+            }
+          }
+        ]
+      }
+    }
+
+  }
+
+  tags = local.tags
+}
+EOF
+
+```
+
+```bash
+cat <<'EOF' >> ./environment/eks-blueprint/modules/eks_cluster/main.tf
+module "eks_blueprints_dev_teams" {
+  source  = "aws-ia/eks-blueprints-teams/aws"
+  version = "~> 0.2"
+
+  for_each = {
+    burnham = {
+      labels = {
+        "elbv2.k8s.aws/pod-readiness-gate-inject" = "enabled",
+        "appName"                                 = "burnham-team-app",
+        "projectName"                             = "project-burnham",
+      }
+    }
+    riker = {
+      labels = {
+        "elbv2.k8s.aws/pod-readiness-gate-inject" = "enabled",
+        "appName"                                 = "riker-team-app",
+        "projectName"                             = "project-riker",
+      }
+    }
+  }
+  name = "team-${each.key}"
+
+  users             = [data.aws_caller_identity.current.arn]
+  cluster_arn       = module.eks.cluster_arn
+  oidc_provider_arn = module.eks.oidc_provider_arn
+
+  labels = merge(
+    {
+      team = each.key
+    },
+    try(each.value.labels, {})
+  )
+
+  annotations = {
+    team = each.key
+  }
+
+  namespaces = {
+    "team-${each.key}" = {
+      labels = merge(
+        {
+          team = each.key
+        },
+        try(each.value.labels, {})
+      )
+
+      resource_quota = {
+        hard = {
+          "requests.cpu"    = "100",
+          "requests.memory" = "20Gi",
+          "limits.cpu"      = "200",
+          "limits.memory"   = "50Gi",
+          "pods"            = "15",
+          "secrets"         = "10",
+          "services"        = "20"
+        }
+      }
+
+      limit_range = {
+        limit = [
+          {
+            type = "Pod"
+            max = {
+              cpu    = "2"
+              memory = "1Gi"
+            }
+            min = {
+              cpu    = "10m"
+              memory = "4Mi"
+            }
+          },
+          {
+            type = "PersistentVolumeClaim"
+            min = {
+              storage = "24M"
+            }
+          },
+          {
+            type = "Container"
+            default = {
+              cpu    = "50m"
+              memory = "24Mi"
+            }
+          }
+        ]
+      }
+    }
+  }
+
+  tags = local.tags
+
+}
+
+EOF
+
+```
+
+```bash
+cat <<'EOF' >> ./environment/eks-blueprint/modules/eks_cluster/outputs.tf
+
+output "eks_blueprints_platform_teams_configure_kubectl" {
+  description = "Configure kubectl for Platform Team: make sure you're logged in with the correct AWS profile and run the following command to update your kubeconfig"
+  value       = "aws eks --region ${var.aws_region} update-kubeconfig --name ${module.eks.cluster_name}  --role-arn ${module.eks_blueprints_platform_teams.iam_role_arn}"
+}
+
+output "eks_blueprints_dev_teams_configure_kubectl" {
+  description = "Configure kubectl for each Dev Application Teams: make sure you're logged in with the correct AWS profile and run the following command to update your kubeconfig"
+  value       = [for team in module.eks_blueprints_dev_teams : "aws eks --region ${var.aws_region} update-kubeconfig --name ${module.eks.cluster_name}  --role-arn ${team.iam_role_arn}"]
+}
+
+EOF
+
+```
+
+```bash
+cat <<'EOF' >> ./environment/eks-blueprint/eks-blue/outputs.tf
+
+output "eks_blueprints_platform_teams_configure_kubectl" {
+  description = "Configure kubectl for Platform Team: make sure you're logged in with the correct AWS profile and run the following command to update your kubeconfig"
+  value       = module.eks_cluster.eks_blueprints_platform_teams_configure_kubectl
+}
+
+output "eks_blueprints_dev_teams_configure_kubectl" {
+  description = "Configure kubectl for each Dev Application Teams: make sure you're logged in with the correct AWS profile and run the following command to update your kubeconfig"
+  value       = module.eks_cluster.eks_blueprints_dev_teams_configure_kubectl
+}
+
+EOF
+
+```
--- environment/eks-blueprint/modules/eks_cluster/main.tf
+++ environment/eks-blueprint/modules/eks_cluster/main.tf
@@ -84,24 +84,25 @@ module "eks" {
     }
   }
 
-  # manage_aws_auth_configmap = true
-  # aws_auth_roles = flatten([
-  #   #module.eks_blueprints_platform_teams.aws_auth_configmap_role,
-  #   #[for team in module.eks_blueprints_dev_teams : team.aws_auth_configmap_role],
-  #   #{
-  #   #  rolearn  = module.karpenter.role_arn
-  #   #  username = "system:node:{{EC2PrivateDNSName}}"
-  #   #  groups = [
-  #   #    "system:bootstrappers",
-  #   #    "system:nodes",
-  #   #  ]
-  #   #},
-  #   {
-  #     rolearn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${local.eks_admin_role_name}" # The ARN of the IAM role
-  #     username = "ops-role"                                                                                      # The user name within Kubernetes to map to the IAM role
-  #     groups   = ["system:masters"]                                                                              # A list of groups within Kubernetes to which the role is mapped; Checkout K8s Role and Rolebindings
-  #   }
-  # ])
+  manage_aws_auth_configmap = true
+  aws_auth_roles = flatten([
+    #ADD OUR TEAM ROLE TO THE ALLOWED LIST TO AUTHENTICATE IN THE EKS CLUSTER
+    module.eks_blueprints_platform_teams.aws_auth_configmap_role, #ADD OUR TEAM ROLE TO THE ALLOWED LIST TO AUTHENTICATE IN THE EKS CLUSTER
+    [for team in module.eks_blueprints_dev_teams : team.aws_auth_configmap_role], #ADD OUR TEAM ROLE TO THE ALLOWED LIST TO AUTHENTICATE IN THE EKS CLUSTER
+    #{
+    #  rolearn  = module.karpenter.role_arn
+    #  username = "system:node:{{EC2PrivateDNSName}}"
+    #  groups = [
+    #    "system:bootstrappers",
+    #    "system:nodes",
+    #  ]
+    #},
+    {
+      rolearn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${local.eks_admin_role_name}" # The ARN of the IAM role
+      username = "ops-role"                                                                                      # The user name within Kubernetes to map to the IAM role
+      groups   = ["system:masters"]                                                                              # A list of groups within Kubernetes to which the role is mapped; Checkout K8s Role and Rolebindings
+    }
+  ])
 
   tags = merge(local.tags, {
     # NOTE - if creating multiple security groups with this module, only tag the
@@ -109,5 +110,176 @@ module "eks" {
     # (i.e. - at most, only one security group should have this tag in your account)
     "karpenter.sh/discovery" = "${local.environment}-${local.service}"
   })
+}
+
+
+#ADD PLATFORM TEAM
+data "aws_iam_role" "eks_admin_role_name" {
+  count     = local.eks_admin_role_name != "" ? 1 : 0
+  name = local.eks_admin_role_name
+}
+
+module "eks_blueprints_platform_teams" {
+  source  = "aws-ia/eks-blueprints-teams/aws"
+  version = "~> 0.2"
+
+  name = "team-platform"
+
+  # Enables elevated, admin privileges for this team
+  enable_admin = true
+ 
+  # Define who can impersonate the team-platform Role
+  users             = [
+    data.aws_caller_identity.current.arn,
+    try(data.aws_iam_role.eks_admin_role_name[0].arn, data.aws_caller_identity.current.arn),
+  ]
+  cluster_arn       = module.eks.cluster_arn
+  oidc_provider_arn = module.eks.oidc_provider_arn
+
+  labels = {
+    "elbv2.k8s.aws/pod-readiness-gate-inject" = "enabled",
+    "appName"                                 = "platform-team-app",
+    "projectName"                             = "project-platform",
+  }
+
+  annotations = {
+    team = "platform"
+  }
+
+  namespaces = {
+    "team-platform" = {
+
+      resource_quota = {
+        hard = {
+          "requests.cpu"    = "10000m",
+          "requests.memory" = "20Gi",
+          "limits.cpu"      = "20000m",
+          "limits.memory"   = "50Gi",
+          "pods"            = "20",
+          "secrets"         = "20",
+          "services"        = "20"
+        }
+      }
+
+      limit_range = {
+        limit = [
+          {
+            type = "Pod"
+            max = {
+              cpu    = "1000m"
+              memory = "1Gi"
+            },
+            min = {
+              cpu    = "10m"
+              memory = "4Mi"
+            }
+          },
+          {
+            type = "PersistentVolumeClaim"
+            min = {
+              storage = "24M"
+            }
+          }
+        ]
+      }
+    }
+
+  }
+
+  tags = local.tags
+}
+
+#ADD DEV TEAMS
+module "eks_blueprints_dev_teams" {
+  source  = "aws-ia/eks-blueprints-teams/aws"
+  version = "~> 0.2"
+
+  for_each = {
+    burnham = {
+      labels = {
+        "elbv2.k8s.aws/pod-readiness-gate-inject" = "enabled",
+        "appName"                                 = "burnham-team-app",
+        "projectName"                             = "project-burnham",
+      }
+    }
+    riker = {
+      labels = {
+        "elbv2.k8s.aws/pod-readiness-gate-inject" = "enabled",
+        "appName"                                 = "riker-team-app",
+        "projectName"                             = "project-riker",
+      }
+    }
+  }
+  name = "team-${each.key}"
+
+  users             = [data.aws_caller_identity.current.arn]
+  cluster_arn       = module.eks.cluster_arn
+  oidc_provider_arn = module.eks.oidc_provider_arn
+
+  labels = merge(
+    {
+      team = each.key
+    },
+    try(each.value.labels, {})
+  )
+
+  annotations = {
+    team = each.key
+  }
+
+  namespaces = {
+    "team-${each.key}" = {
+      labels = merge(
+        {
+          team = each.key
+        },
+        try(each.value.labels, {})
+      )
+
+      resource_quota = {
+        hard = {
+          "requests.cpu"    = "100",
+          "requests.memory" = "20Gi",
+          "limits.cpu"      = "200",
+          "limits.memory"   = "50Gi",
+          "pods"            = "15",
+          "secrets"         = "10",
+          "services"        = "20"
+        }
+      }
+
+      limit_range = {
+        limit = [
+          {
+            type = "Pod"
+            max = {
+              cpu    = "2"
+              memory = "1Gi"
+            }
+            min = {
+              cpu    = "10m"
+              memory = "4Mi"
+            }
+          },
+          {
+            type = "PersistentVolumeClaim"
+            min = {
+              storage = "24M"
+            }
+          },
+          {
+            type = "Container"
+            default = {
+              cpu    = "50m"
+              memory = "24Mi"
+            }
+          }
+        ]
+      }
+    }
+  }
+
+  tags = local.tags
+
 }
 
